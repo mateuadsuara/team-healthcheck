cd96d04862fd2fc214ab7fba97315e834cfc6b37
Mateu Adsuara <mateuadsuara@gmail.com>
2019-01-05 02:42:20 +0000

    Read index.html at compiletime

--- backend/lib/backend/router.ex
+++ backend/lib/backend/router.ex
@@ -1,19 +1,17 @@
 defmodule Backend.Router do
+  {:ok, index_html} = File.read "../frontend/_build/index.html"
+  @index_html index_html
+
   use Plug.Router
 
   plug(:match)
   plug(:dispatch)
 
   get "/" do
-    send_resp(conn, 200, index_html())
+    send_resp(conn, 200, @index_html)
   end
 
   match _ do
     send_resp(conn, 404, "Oops!")
-  end
-
-  defp index_html() do
-    {:ok, index_html} = File.read "../frontend/_build/index.html"
-    index_html
   end
 end
