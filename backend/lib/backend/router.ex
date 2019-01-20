053239400430dd42fc71a8effa8111ec5892617a
Mateu Adsuara <mateuadsuara@gmail.com>
2019-01-20 01:35:00 +0000

    Add favicon

--- backend/lib/backend/router.ex
+++ backend/lib/backend/router.ex
@@ -2,6 +2,9 @@ defmodule Backend.Router do
   {:ok, index_html} = File.read "../frontend/_build/index.html"
   @index_html index_html
 
+  {:ok, favicon} = File.read "../frontend/html/favicon.ico"
+  @favicon favicon
+
   use Plug.Router
 
   plug(:match)
@@ -9,6 +12,10 @@ defmodule Backend.Router do
 
   get "/" do
     send_resp(conn, 200, @index_html)
+  end
+
+  get "/favicon.ico" do
+    send_resp(conn, 200, @favicon)
   end
 
   match _ do
