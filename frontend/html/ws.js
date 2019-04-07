4aa2984d5dff5e9ec7ba523ace4794cbdde7a94f
Mateu Adsuara <mateuadsuara@gmail.com>
2019-04-07 17:34:57 +0000

    Add reconnection strategy

--- frontend/html/ws.js
+++ frontend/html/ws.js
@@ -4,6 +4,8 @@ class WS {
     options.host = options.host || window.location.host
     options.pathname = options.pathname || '/websocket'
     options.protocol = options.protocol || window.location.protocol === 'https:' ? 'wss:' : 'ws:'
+    options.retries = options.retries || 60
+    options.retryDelay = options.retryDelay || 1000
     options.timeout = options.timeout || 5000
     options.pingInterval = options.pingInterval || 50000
     this.options = options
@@ -12,6 +14,10 @@ class WS {
   }
 
   connect() {
+    return this._connect(this.options.retries)
+  }
+
+  _connect(retriesLeft) {
     const that = this
 
     const options = that.options
@@ -23,15 +29,35 @@ class WS {
         console.log(event)
       })
       that.socket.addEventListener("close", () => {
-        console.log("CLOSE")
-      })
-      return attachPingStrategy(socket, options.pingInterval)
-        .then((pingStrategy) => {
-            pingStrategy.addEventListener("tick", (event) => {
-              console.log(event.detail)
-            })
-            return that
+        console.log("Websocket closed!")
+        that._connect(0).then(() => {
+          console.log("Websocket reconnected immediately!")
+        }).catch(() => {
+          that._connect(that.options.retries -1).then(() => {
+            console.log("Websocket reconnected with trouble!")
+          }).catch(() => {
+            console.log("Websocket FATAL!")
+          })
         })
+      })
+      const pingStrategy = attachPingStrategy(socket, options.pingInterval)
+      pingStrategy.addEventListener("tick", (event) => {
+        console.log(event.detail)
+      })
+      return that
+    }).catch((error) => {
+      if (retriesLeft === 0) {
+        console.log("Failed connecting to websocket!")
+        throw error
+      } else {
+        return new Promise((resolve, reject) => {
+          const delay = options.retryDelay
+          console.log(`Failed connecting to websocket! retrying in ${delay} milliseconds (${retriesLeft} retries left)`)
+          setTimeout(() => {
+            that._connect(retriesLeft -1).then(resolve, reject)
+          }, delay)
+        })
+      }
     })
   }
 
@@ -41,76 +67,72 @@ class WS {
 }
 
 function attachPingStrategy(socket, interval) {
-  return new Promise((resolve, reject) => {
+  const eventTarget = new EventTarget()
+
+  const tick = (data) => {
+    eventTarget.dispatchEvent(new CustomEvent("tick", {
+      detail: data
+    }))
+  }
+
+  var ping
+  ping = () => {
     if (socket.readyState !== WebSocket.OPEN) {
-      reject(new Error("Socket is not open"))
-    } else {
-      const eventTarget = new EventTarget()
-
-      const tick = (data) => {
-        eventTarget.dispatchEvent(new CustomEvent("tick", {
-          detail: data
-        }))
-      }
-
-      var ping
-      ping = () => {
-        if (socket.readyState !== WebSocket.OPEN) {
-          return
-        }
-
-        const startTime = new Date().getTime()
-        const id = startTime
-
-        var pingTimeout
-
-        const pongListener = (event) => {
-          const data = JSON.parse(event.data)
-          if (data.pong === id) {
-            clearTimeout(pingTimeout)
-            socket.removeEventListener("message", pongListener)
-            const endTime = new Date().getTime()
-            const latency = endTime - startTime
-            tick({
-              ping: startTime,
-              pong: endTime,
-              latency: latency,
-              interval: interval
-            })
-            setTimeout(ping, interval - latency)
-          }
-        }
-        socket.addEventListener("message", pongListener)
-
-        socket.send(JSON.stringify({ping: id}))
-
-        pingTimeout = setTimeout(() => {
-          socket.removeEventListener("message", pongListener)
-          const endTime = new Date().getTime()
-          tick({
-            ping: startTime,
-            timeout: endTime,
-            interval: interval
-          })
-          ping()
-        }, interval)
-      }
-      ping()
-
-      resolve(eventTarget)
+      return
     }
-  })
+
+    const startTime = new Date().getTime()
+    const id = startTime
+
+    var pingTimeout
+
+    const pongListener = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.pong === id) {
+        clearTimeout(pingTimeout)
+        socket.removeEventListener("message", pongListener)
+        const endTime = new Date().getTime()
+        const latency = endTime - startTime
+        tick({
+          ping: startTime,
+          pong: endTime,
+          latency: latency,
+          interval: interval
+        })
+        setTimeout(ping, interval - latency)
+      }
+    }
+    socket.addEventListener("message", pongListener)
+
+    socket.send(JSON.stringify({ping: id}))
+
+    pingTimeout = setTimeout(() => {
+      socket.removeEventListener("message", pongListener)
+      const endTime = new Date().getTime()
+      tick({
+        ping: startTime,
+        timeout: endTime,
+        interval: interval
+      })
+      ping()
+    }, interval)
+  }
+  ping()
+
+  return eventTarget
 }
 
 function openSocket(path, timeout) {
   return new Promise((resolve, reject) => {
     const socket = new WebSocket(path)
 
-    var connectionTimeoutId, readyIntervalCheckId
-    connectionTimeoutId = setTimeout(() => {
+    var readyIntervalCheckId
+
+    const connectionTimeoutId = setTimeout(() => {
       clearInterval(readyIntervalCheckId)
       reject(new Error("Connection timeout"))
     }, timeout)
+
     readyIntervalCheckId = setInterval(() => {
       if (socket.readyState === WebSocket.OPEN) {
         clearTimeout(connectionTimeoutId)
@@ -121,7 +143,7 @@ function openSocket(path, timeout) {
         clearInterval(readyIntervalCheckId)
         reject(new Error("The socket got into an unexpected state: " + socket.readyState))
       }
-    }, 0)
+    }, 10)
   })
 }
 
